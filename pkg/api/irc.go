dcc4ea7a36ea52a46756d66446a51ac3dfaa64d7
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-01-15 07:20:05 +0000

    Add autosr

--- pkg/api/irc.go
+++ pkg/api/irc.go
@@ -18,6 +18,7 @@ var host = "wss://irc-ws.chat.twitch.tv"
 var logirc = log.New(os.Stdout, "IRC ", log.Ldate|log.Ltime)
 var irc *IRCConn = nil
 var connectRetries = 0
+var autosr = false
 
 var msgChan *chan string
 
@@ -40,8 +41,8 @@ func (irc *IRCConn) IRCMessage(s string) {
 	websocket.Message.Send(irc.Conn, s)
 }
 
-func chat(s string) string {
-	return "PRIVMSG " + data.AppCfg.TwitchChannel + " :" + s
+func chat(s string, channel string) string {
+	return "PRIVMSG " + channel + " :" + s
 }
 
 func Run(exitCh *chan struct{}) {
@@ -84,36 +85,93 @@ func Run(exitCh *chan struct{}) {
 		}
 		stringmsg := string(msg[:n])
 		for _, v := range strings.Split(stringmsg, "\r\n") {
-			go processIRC(irc, v, n)
+			if v != "" {
+				logirc.Println(v)
+				go processIRC(irc, v, n)
+			}
 		}
 	}
 }
 
 func processIRC(irc *IRCConn, incoming string, n int) {
+	breakdown := strings.Split(incoming, " ")
+	identity := breakdown[0]
+	user := ""
+	if len(breakdown) > 1 {
+		user = breakdown[1]
+	}
+	// incomingType := ""
+	// if len(breakdown) > 1 {
+	// 	incomingType = breakdown[2]
+	// }
+	incomingChannel := ""
+	if len(breakdown) > 3 {
+		incomingChannel = breakdown[3]
+	}
+	actualMessage := ""
+	if len(breakdown) > 4 {
+		actualMessage = strings.Join(breakdown[4:], " ")[1:]
+	}
+
 	switch {
 	case strings.HasPrefix(incoming, "PING"):
 		*msgChan <- strings.Replace(incoming, "PING", "PONG", 1)
 		logirc.Println(strings.Replace(incoming, "PING", "PONG", 1))
-	case strings.HasPrefix(incoming, "@ban-duration="):
-		fallthrough
-	case strings.Contains(incoming, " CLEARCHAT "):
+	case strings.Contains(incoming, " CLEARCHAT ") || strings.HasPrefix(incoming, "@ban-duration="):
 		// handleBan("@ban-duration=1;room-id=254067565;target-user-id=19609203;tmi-sent-ts=1673678936710 :tmi.twitch.tv CLEARCHAT #sangnope :omnoloko")
-		handleBan(incoming)
+		handleBan(incoming, incomingChannel)
+	case strings.Contains(identity, "custom-reward-id="):
+		handleRewards(identity, incomingChannel, user, actualMessage)
+		// bts blood sweat tears
+	case strings.Contains(identity, "mod=1") && strings.HasPrefix(actualMessage, "!"):
+		handleCommand(incomingChannel, user, actualMessage)
 	default:
 		logirc.Println(incoming)
 	}
 }
 
-func handleBan(msg string) {
-	bannedUser := msg[strings.LastIndex(msg, ":")+1:]
-	perm := !strings.HasPrefix(msg, "@ban-duration=")
+func IdentityParser(identity string) map[string]string {
+	r := make(map[string]string)
+	for _, group := range strings.Split(identity, ";") {
+		v := strings.Split(group, "=")
+		r[v[0]] = v[1]
+	}
+	return r
+}
+
+func handleRewards(identity string, incomingChannel string, user string, actualMesage string) {
+	rewardsMap := IdentityParser(identity)
+	if _, ok := rewardsMap[incomingChannel]; !ok {
+		return
+	}
+	if !strings.Contains(identity, rewardsMap[incomingChannel]) {
+		return
+	}
+	// Per channel implementation. For now only Erica's
+	if autosr {
+		*msgChan <- chat("!sr "+actualMesage, incomingChannel)
+	}
+}
+
+func handleCommand(incomingChannel string, user string, acutalMessage string) {
+	if strings.HasPrefix(acutalMessage, "!autosr") {
+		autosr = !autosr
+	}
+}
+
+func handleBan(rawmsg string, channel string) {
+	if !strings.Contains(data.AppCfg.AutoUnbanChannels, channel) {
+		return
+	}
+	bannedUser := rawmsg[strings.LastIndex(rawmsg, ":")+1:]
+	perm := !strings.HasPrefix(rawmsg, "@ban-duration=")
 
 	if strings.Contains(data.AppCfg.AutoUnbans, bannedUser) {
 		logirc.Println("Unbanning " + bannedUser)
 		if perm {
-			*msgChan <- chat("/unban " + bannedUser)
+			*msgChan <- chat("/unban "+bannedUser, channel)
 		} else {
-			*msgChan <- chat("/untimeout " + bannedUser)
+			*msgChan <- chat("/untimeout "+bannedUser, channel)
 		}
 		// fluff here
 	}
--- pkg/data/ConfigDatabase.go
+++ pkg/data/ConfigDatabase.go
@@ -1,10 +1,13 @@
 package data
 
 type ConfigDatabase struct {
-	TwitchChannel         string `env:"TWITCH_CHANNEL"`
-	TwitchAccount         string `env:"TWITCH_ACCOUNT"`
-	TwitchPassword        string `env:"TWITCH_PASSWORD"`
-	AutoUnbans            string `env:"AUTO_UNBANS"`
+	TwitchChannel          string `env:"TWITCH_CHANNEL"`
+	TwitchAccount          string `env:"TWITCH_ACCOUNT"`
+	TwitchPassword         string `env:"TWITCH_PASSWORD"`
+	AutoUnbans             string `env:"AUTO_UNBANS"`
+	AutoUnbanChannels      string `env:"AUTO_UNBAN_CHANNELS"`
+	AutoSongRequestChannel string `env:"AUTO_SONG_REQUEST_CHANNEL"`
+	AutoSongRequestID      string `env:"AUTO_SONG_REQUEST_ID"`
 }
 
 var AppCfg ConfigDatabase
