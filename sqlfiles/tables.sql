8195b64a677921029d3030bbcbf39f274e9c3dd2
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-03-04 05:52:17 +0000

    Add new data structure notes

--- sqlfiles/tables.sql
+++ sqlfiles/tables.sql
@@ -7,3 +7,56 @@ CREATE TABLE IF NOT EXISTS sangnope (
 
 INSERT INTO sangnope VALUES (1, 0, 'dumpy count', '!dumpy');
 /* UPDATE sangnope set num = (num + 1) where id = 1 returning num; */
+
+/*
+
+All states:
+disabled commands
+autosr
+rewardsMap
+
+cache in ram, dont store in db:
+lastBanTime
+command cooldowns
+
+
+db structure
+
+channels
+  id
+  channel
+
+channel_commands
+  channel_command_id
+  channel_id
+  command
+  special T/F
+  all
+  subs
+  mods
+  super
+  
+
+channel_command_perm_overrides
+  channel_command_id
+  user
+  allowed (F for deny)
+
+redis cache structure
+
+#channel_state
+  autosr
+  disabled commands
+
+data loading flow
+  Init db connection
+  query db for all channels
+  fetch channel state from redis, and set those, update those as the app is executed
+
+  when command happens it pings db every single time? (yea its fine considering it was that way at LF)
+    command timeouts should be kept in ram, not cached, not db.
+    permissions should be fetched at the same time with a left join
+  
+  Thats it
+
+*/
